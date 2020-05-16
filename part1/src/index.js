e87e9b8bdff458b3d396259f66bd678a24f7eed4
adnanj <adnan.webdev@gmail.com>
2020-05-16 23:51:49 +1000

    dleleted all files due to eslint error

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => (
-  <div>
-    <p> Hello World</p>
-  </div>
-);
+
+const course = "halfs stack application development";
+const part1 = "Fundamentals of Reacct";
+const exercises1 = 10;
+const part2 = "Using props to pass data";
+const exercises2 = 7;
+const part3 = "State of component";
+const exercises3 = 14;
+
+
+<div>
+  <h1>{course}</h1>
+  <p>{part1}{exercises1}</p>
+  <p>{part2}{exercises2}</p>
+  <p>{part3}{exercises3}</p>
+  <p>No of exercise {exercises1 + exercises2 + exercises3}</p>
+</div>
 
 ReactDOM.render(<App />, document.getElementById("root"));
